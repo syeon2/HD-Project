1113687f206cc8b31fe2d760cfa9f8308c672980
SUYEON KIM <waterkite94@gmail.com>
2023-09-01 16:20:26 +0900

    Project INIT

---
+++ src/main/java/personal/hdproject/HdProjectApplication.java
@@ -0,0 +1,13 @@
+package personal.hdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdProjectApplication.class, args);
+	}
+
+}
