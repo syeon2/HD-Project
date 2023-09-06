5b0ae03b24aa0b22127ef4d9ae982a912b746268
SUYEON KIM <waterkite94@gmail.com>
2023-09-06 22:49:38 +0900

    [#2] Feat: Add Error Handler
    - Add Error Exception and Handler : 아이디 중복 예외 클래스를 추가

--- src/main/java/personal/hdproject/util/error/ErrorController.java
+++ src/main/java/personal/hdproject/util/error/ErrorController.java
@@ -5,10 +5,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import personal.hdproject.util.error.exception.DuplicateEmailException;
 import personal.hdproject.util.wrapper.ApiResult;
 
 @RestControllerAdvice
 public class ErrorController {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(DuplicateEmailException.class)
+	public ApiResult<Void> handlerDuplicateEmailRequest(DuplicateEmailException exception) {
+		return ApiResult.onFailure(exception.getLocalizedMessage());
+	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(RuntimeException.class)
