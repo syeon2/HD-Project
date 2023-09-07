9f962046c58e869aa9beaa271b4f72a928bcd924
SUYEON KIM <waterkite94@gmail.com>
2023-09-07 14:02:56 +0900

    [#2] Feat: Add Validation Exception
    - Add : RequestBody로 받을 DTO의 필드를 Validation하고 예외 시 처리할 예외 클래스 추가

--- src/main/java/personal/hdproject/util/error/ErrorController.java
+++ src/main/java/personal/hdproject/util/error/ErrorController.java
@@ -1,6 +1,7 @@
 package personal.hdproject.util.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,12 @@ public class ErrorController {
 	@ExceptionHandler(DuplicateEmailException.class)
 	public ApiResult<Void> handlerDuplicateEmailRequest(DuplicateEmailException exception) {
 		return ApiResult.onFailure(exception.getLocalizedMessage());
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ApiResult<Void> handlerValidationRequest(MethodArgumentNotValidException e) {
+		return ApiResult.onFailure(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
